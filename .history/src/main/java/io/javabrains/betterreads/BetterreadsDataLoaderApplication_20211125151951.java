e1a12b17b875a4c5a9879f97d0b7ee64463e59aa
Eidan <eidankhan.student.muet@gmail.com>
2021-11-25 15:27:43 +0500

    Connecting Spring Boot app to DataStax Astra

---
+++ .history/src/main/java/io/javabrains/betterreads/BetterreadsDataLoaderApplication_20211125151951.java
@@ -0,0 +1,27 @@
+package io.javabrains.betterreads;
+
+
+import java.nio.file.Path;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class BetterreadsDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
+		System.out.println("App is running ...!");
+	}
+
+	// @Bean
+    // public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+    //     Path bundle = astraProperties.getSecureConnectBundle().toPath();
+    //     return builder -> builder.withCloudSecureConnectBundle(bundle);
+    // }
+
+}
